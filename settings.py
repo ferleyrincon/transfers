03d650c2c62211e8adbccf37fdf6ba2d39f5b324
Jorge Saul <jorge.castillo1@correo.uis.edu.co>
2022-02-16 07:27:33 -0500

    add eet settings

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 from os import environ
 
 SESSION_CONFIGS = [
-    # dict(
-    #    name='public_goods',
-    #    display_name="Public Goods",
-    #    num_demo_participants=3,
-    #    app_sequence=['public_goods', 'payment_info']
-    # ),
+    dict(
+       name='eet',
+       display_name="eet",
+       num_demo_participants=2,
+       app_sequence=['eet']
+    ),
 ]
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
